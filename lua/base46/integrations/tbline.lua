1642521b625706fc371b40f3c6f35503f400cba8
jianshanbushishan <jianshanbushishan@qq.com>
2025-04-03 17:14:22 +0800

    调整tabline高亮集成

--- lua/base46/integrations/tbline.lua
+++ lua/base46/integrations/tbline.lua
@@ -11,15 +11,12 @@ function M.GetHighlight(themeColors)
 
     TbBufOnModified = { fg = base30.green, bg = base30.black },
     TbBufOffModified = { fg = base30.red, bg = base30.black2 },
-    TbBufOnClose = { fg = base30.red, bg = base30.black },
-    TbBufOffClose = { fg = base30.light_grey, bg = base30.black2 },
+    BufSepOn = { fg = base30.red, bg = base30.black },
+    BufSepOff = { fg = base30.light_grey, bg = base30.black2 },
     TbTabNewBtn = { fg = base30.white, bg = base30.one_bg2 },
     TbTabOn = { fg = base30.red },
     TbTabOff = { fg = base30.white, bg = base30.black2 },
-    TbTabCloseBtn = { fg = base30.black, bg = base30.nord_blue },
     TBTabTitle = { fg = base30.black, bg = base30.blue },
-    TbThemeToggleBtn = { bold = true, fg = base30.white, bg = base30.one_bg3 },
-    TbCloseAllBufsBtn = { bold = true, bg = base30.red, fg = base30.black },
   }
 end
 
