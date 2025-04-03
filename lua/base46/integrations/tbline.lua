6406103dc097a5b25a68ff631aedd3b5d5aa4b88
jianshanbushishan <jianshanbushishan@qq.com>
2025-04-03 17:32:37 +0800

    修改tbline样式

--- lua/base46/integrations/tbline.lua
+++ lua/base46/integrations/tbline.lua
@@ -6,17 +6,15 @@ function M.GetHighlight(themeColors)
   return {
     Tabline = { bg = base30.black2 },
     TbFill = { bg = base30.black2 },
-    TbBufOn = { fg = base30.white, bg = base30.black },
+
+    TbBufOn = { fg = base30.white, bg = base30.black, underline = true },
     TbBufOff = { fg = base30.light_grey, bg = base30.black2 },
 
     TbBufOnModified = { fg = base30.green, bg = base30.black },
     TbBufOffModified = { fg = base30.red, bg = base30.black2 },
+
     BufSepOn = { fg = base30.red, bg = base30.black },
     BufSepOff = { fg = base30.light_grey, bg = base30.black2 },
-    TbTabNewBtn = { fg = base30.white, bg = base30.one_bg2 },
-    TbTabOn = { fg = base30.red },
-    TbTabOff = { fg = base30.white, bg = base30.black2 },
-    TBTabTitle = { fg = base30.black, bg = base30.blue },
   }
 end
 
